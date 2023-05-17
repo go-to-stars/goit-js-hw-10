ee89a1e171146e5ed51aa1f878dc4bea374a0053
go-to-stars <ivanstep@ukr.net>
2023-05-17 20:56:27 +0300

    add library

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
