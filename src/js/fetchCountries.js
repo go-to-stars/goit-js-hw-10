d08352b86065bccba457fd40d36bdfc8c724d74b
go-to-stars <ivanstep@ukr.net>
2023-05-20 02:04:30 +0300

    add script function fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,10 @@
-export function fetchCountries(params) {
-  console.log(params);
-}
+export function fetchCountries(nameCountry) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${nameCountry}?fields=flags,name,capital,population,languages,currencies`
+  ).then(responce => {
+    if (!responce.ok) {
+      throw new Error(responce.status); //обробка та передача помилки (404)
+    }
+    return responce.json(); // чейніннг до промісу відповіді
+  }); // повертає проміс запиту
+} // іменований експорт функції fetchCountries
